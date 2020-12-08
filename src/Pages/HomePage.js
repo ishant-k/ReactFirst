ecfdb7c86006d8f3b34a82f753128ff323fc40af
ishant-k <74232790+ishant-k@users.noreply.github.com>
2020-12-08 13:50:53 +0530

    third

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -5,6 +5,20 @@ return (
 <>
 <h1>Hey from HomePage</h1>
 <p>This is your awesome HomePage subtitle</p>
+
+<header class="w3-container w3-xlarge">
+    <p class="w3-left">Jeans</p>
+</header>
+
+<div class="w3-display-container w3-container">
+    <img src="https://www.w3schools.com/w3images/jeans.jpg" alt="Jeans"></img>
+    <div class="w3-display-topleft w3-text-white">
+        <h1 class="w3-jumbo w3-hide-small">New arrivals</h1>
+        <h1 class="w3-hide-small">COLLECTION 2016</h1>
+        <p><a href="/contact" class="w3-button w3-black w3-padding-large w3-large">SHOP NOW</a></p>
+    </div>
+</div>
+
 </>
 );
 }
